e7107f89fa99a5e4fbbc4ae4b0c86ede629423a0
karpusha <vkarpusha@gmail.com>
2021-06-27 12:40:33 +0300

    Update with latest changes 3

--- etchash.go
+++ etchash.go
@@ -462,14 +462,14 @@ func (l *Light) Verify(block Block) bool {
 }
 
 // compute() to get mixhash and result
-func (l *Light) Compute(blockNum uint64, hashNoNonce common.Hash, nonce uint64) (mixDigest common.Hash, result common.Hash) {
-	epochLength := calcEpochLength(blockNum, l.ecip1099FBlock)
-	epoch := calcEpoch(blockNum, epochLength)
-
-	cache := l.getCache(blockNum)
-	dagSize := datasetSize(epoch)
-	return cache.compute(uint64(dagSize), hashNoNonce, nonce)
-}
+// func (l *Light) Compute(blockNum uint64, hashNoNonce common.Hash, nonce uint64) (mixDigest common.Hash, result common.Hash) {
+// 	epochLength := calcEpochLength(blockNum, l.ecip1099FBlock)
+// 	epoch := calcEpoch(blockNum, epochLength)
+//
+// 	cache := l.getCache(blockNum)
+// 	dagSize := datasetSize(epoch)
+// 	return cache.compute(uint64(dagSize), hashNoNonce, nonce)
+// }
 
 func (l *Light) getCache(blockNum uint64) *cache {
 	var c *cache
